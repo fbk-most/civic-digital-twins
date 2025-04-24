4266f7d8efdb0a0b4af8b875e1a1e362db13aaf3
Simone Basso <bassosimone@gmail.com>
2025-04-24 14:05:57 +0200

    fix: type correctness around symbols and context variables
    This diff adds type annotations and updates the context variables
    code to make the expectations explicity regarding when we want
    symbols, and when we want context variables.
    
    Additionally, we change the molveno example to include a more
    complex weather situation, to exercise the previous bug.
    
    The related issue is https://github.com/fbk-most/dt-model/issues/37.
    
    If this diff is accepted, I'll add more tests either to this PR
    or to a follow up PR, depending on how we proceed.

--- dt_model/simulation/ensemble.py
+++ dt_model/simulation/ensemble.py
@@ -4,9 +4,9 @@ from __future__ import annotations
 
 from functools import reduce
 
-from dt_model.internal.sympyke.symbol import SymbolValue
+from dt_model.model.model import Model
 from dt_model.symbols.context_variable import ContextVariable
-from dt_model.model.instantiated_model import InstantiatedModel
+from dt_model.internal.sympyke.symbol import SymbolValue
 
 
 class Ensemble:
@@ -14,7 +14,7 @@ class Ensemble:
 
     def __init__(
         self,
-        model: InstantiatedModel,
+        model: Model,
         scenario: dict[ContextVariable, list[SymbolValue]],
         cv_ensemble_size: int = 20,
     ):
