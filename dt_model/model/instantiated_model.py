a69010c1c38495735583e1820950b5d1131ae478
Marco Pistore <marco.pistore@gmail.com>
2025-04-29 18:26:36 +0200

    X

--- dt_model/model/instantiated_model.py
+++ dt_model/model/instantiated_model.py
@@ -7,7 +7,7 @@ from dt_model.model.legacy_model import LegacyModel
 class InstantiatedModel:
     """Instantiation of AbstractModel."""
 
-    def __init__(self, abs: AbstractModel, name: str = None, values: dict = None) -> None:
+    def __init__(self, abs: AbstractModel, name: str | None = None, values: dict | None = None) -> None:
         self.abs = abs
         self.name = name if name is not None else abs.name
         self.values = values
