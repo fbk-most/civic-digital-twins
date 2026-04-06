5462cdad50a160152f485f1f3f2875210af8e4e5
Marco Pistore <marco.pistore@gmail.com>
2026-04-06 21:37:55 +0200

    chore: drop Python 3.11 support (#122)

--- civic_digital_twins/__init__.py
+++ civic_digital_twins/__init__.py
@@ -1,13 +1,1 @@
 """Civic Digital Twins Modelling Framework."""
-
-import sys
-import warnings
-
-# Deprecation warning for Python 3.11
-if sys.version_info < (3, 12):  # pragma: no cover
-    warnings.warn(
-        "Python 3.11 support is deprecated and will be removed in a future version. "
-        "Please upgrade to Python 3.12 or later.",
-        DeprecationWarning,
-        stacklevel=2,
-    )
